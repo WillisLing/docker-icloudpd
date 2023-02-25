73d0d0dab72aa590dc483a3b93d66b7d0cf81f4e
willisling <willisling@live.com>
2023-02-25 16:24:41 +0800

    update container version

--- docker-develop/build.sh
+++ docker-develop/build.sh
@@ -1,2 +1,2 @@
 docker build -t willisling/docker-icloudpd[:TAGNAME] [--no-cache] .
-# docker build -t willisling/docker-icloudpd:1.0.18 willisling/docker-icloudpd:latest --no-cache .
+# docker build -t willisling/docker-icloudpd:1.0.18 -t willisling/docker-icloudpd:latest --no-cache .
