9fefb2e6bcb1646f1e6d951d6d2c2d39dcb53dae
willisling <willisling@live.com>
2023-04-01 18:33:58 +0800

    Update dev tools to support build amd64 image on M1 mac

--- docker-develop/build.sh
+++ docker-develop/build.sh
@@ -1,2 +1,5 @@
-docker build -t willisling/docker-icloudpd[:TAGNAME] [--no-cache] .
-# docker build -t willisling/docker-icloudpd:1.0.18 -t willisling/docker-icloudpd:latest --no-cache .
+# docker build -t willisling/docker-icloudpd[:TAGNAME] [--no-cache] .
+# docker build -t willisling/docker-icloudpd:1.0.27 -t willisling/docker-icloudpd:latest --no-cache .
+
+docker buildx create --use --name=docker-buildx --platform=linux/amd64
+docker buildx build --platform linux/amd64 -t willisling/docker-icloudpd:1.0.27 -t willisling/docker-icloudpd:latest -o type=docker --no-cache .
